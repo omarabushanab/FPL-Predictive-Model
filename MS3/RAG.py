a5f3a87f6719d21fdef343f7d71117f67e1fb218
Omar abu Shanab <omarabushanab03@gmail.com>
2025-12-06 14:10:40 +0200

    initialization MS3

---
+++ MS3/RAG.py
@@ -0,0 +1,15 @@
+## 1. input preprocessing
+# 
+# a. Intent Classification
+
+def input_preprocessing(input):
+
+    intent_classification()
+
+    entity_extraction()
+
+    if(twoB):
+        input_embedding()
+
+def intent_classification():
+    
