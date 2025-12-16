d5575dfee3bb3806a6545a6b747d40a511e4febf
Ahmed H <hisham.ahmedy6u7i8@gmail.com>
2025-12-16 12:41:13 +0200

    bgrb

--- MS3/helpers/test_entity_extraction.py
+++ MS3/helpers/test_entity_extraction.py
@@ -11,21 +11,21 @@ conn = Neo4jConnection(config["URI"], config["USERNAME"], config["PASSWORD"])
 ner = FPLEncoderNER(conn) 
 
 queries = [
-    "Is Haaland a good pick for GW10 this season?",
-    "Show me the best Arsenal defenders and best Manchester United and Manchester city defenders.",
-    "Compare Mohamed Salah's and Saka's and kevin de bruyne from last season Elneny.",
-    "Who scored the most goals for liverpool",
-    "Show me Man City and Chelsea players for gameweek 5 and 6 and gameweek 8 to 11 and gw 11,12,13",
-    "tell me what arsenal player scored most goals and got most assists and best form in 2022/23.",
-    "Compare forwards and midfielders from 2022/23 season",
-    "Compare forwards and defenders from 2020-21 season",
-    "How many minutes did Bruno Fernandes play in the 2022-23 season gameweek 38?",
-    "In season 2022-23 show Bukayo Saka points in gameweek 8",
-    "What were the total points scored by Marcus Rashford in gameweek 15 for the 2022-23 campaign?"
-    "Provide the aggregated stats for Harry Kane during the 2022-23 season",
-    "How many total points did Bukayo Saka achieve in the 2022/23 season chelseas's",
-    "Summarize the season points and minutes for Virgil van Dijk in the 2022-23 campaign goals assists and compare Goalies Attackers Defenders Midfielders Forwards Manchester United manchester city Arsenal', 'Aston Villa', 'Bournemouth', 'Brentford', 'Brighton', 'Burnley', 'Chelsea', 'Crystal Palace', 'Everton', 'Fulham', 'Leeds', 'Leicester', 'Liverpool', 'Man City', 'Man Utd', 'Newcastle', 'Norwich', Nott'm Forest , 'Southampton', hotspurs Watford', 'West Ham', 'wolverhampton' form"
-    "What were Arsenal’s total goals scored, assists, and total points during the season 2021–22 ?"
+    # "Is Haaland a good pick for GW10 this season?",
+    # "Show me the best Arsenal defenders and best Manchester United and Manchester city defenders.",
+    # "Compare Mohamed Salah's and Saka's and kevin de bruyne from last season Elneny.",
+    # "Who scored the most goals for liverpool",
+    # "Show me Man City and Chelsea players for gameweek 5 and 6 and gameweek 8 to 11 and gw 11,12,13",
+    # "tell me what arsenal player scored most goals and got most assists and best form in 2022/23.",
+    # "Compare forwards and midfielders from 2022/23 season",
+    # "Compare forwards and defenders from 2020-21 season",
+    # "How many minutes did Bruno Fernandes play in the 2022-23 season gameweek 38?",
+    # "In season 2022-23 show Bukayo Saka points in gameweek 8",
+    # "What were the total points scored by Marcus Rashford in gameweek 15 for the 2022-23 campaign?"
+    # "Provide the aggregated stats for Harry Kane during the 2022-23 season",
+    # "How many total points did Bukayo Saka achieve in the 2022/23 season chelseas's",
+    # "Summarize the season points and minutes for Virgil van Dijk in the 2022-23 campaign goals assists and compare Goalies Attackers Defenders Midfielders Forwards Manchester United manchester city Arsenal', 'Aston Villa', 'Bournemouth', 'Brentford', 'Brighton', 'Burnley', 'Chelsea', 'Crystal Palace', 'Everton', 'Fulham', 'Leeds', 'Leicester', 'Liverpool', 'Man City', 'Man Utd', 'Newcastle', 'Norwich', Nott'm Forest , 'Southampton', hotspurs Watford', 'West Ham', 'wolverhampton' form"
+    "What were Arsenal’s total goals scored, assists, and total points during the 2021–22 season ?"
 ] 
 
 for q in queries: 
